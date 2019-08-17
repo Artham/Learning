22875082f23a53bab6487b5203440c865aedd494
Laxman <laxman.kumar1204@gmail.com>
2019-08-17 15:39:27 +0530

    updation of scala file 2

--- src/main/scala/org/scala/learning/FunctionVSMethod.scala
+++ src/main/scala/org/scala/learning/FunctionVSMethod.scala
@@ -4,6 +4,7 @@ object FunctionVSMethod {
   def main(args: Array[String]): Unit ={
     println("Testing main method")
     println("second line added")
+    println("second line added 2")
   }
 }
 
